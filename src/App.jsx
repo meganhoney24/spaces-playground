ea5fa6a9ff2b932c92188f4fe023f0ae8c7fc065
Megan Honey <meganhoney@Megans-MacBook-Pro.local>
2020-11-21 17:48:14 -0500

    addedplane

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,29 @@ import {StandardEnvironment, Background, Logo, Interactable, Image} from "spaces
 function App() {
     return (<StandardEnvironment
         player={{
-        pos: new Vector3(0, 1, -48),
+        pos: new Vector3(0, 1, 0),
         rot: Math.PI / 2
     }}
         canvasProps={{
         camera: {
             far: 300
         }
-    }}/>);
+
+    }}>
+        <ambientLight/>
+        <group scale={[2, 2, 2]}> 
+        <mesh rotation={[Math.PI/-2, 0, 0]}>
+                <boxBufferGeometry args={[1,1,1]} />
+                <meshStandardMaterial color="blue"/>
+
+            </mesh>
+            <mesh rotation={[Math.PI/-2, 0, 0]}>
+                <planeBufferGeometry args={[400,400]} />
+                <meshStandardMaterial color="red"/>
+
+            </mesh>
+        </group>
+        </StandardEnvironment>);
 }
 
 export default App;
